2c28b31a8e941aa00273a3e328c270715384ecc5
lionel.wilson <lwilson@cynergybank.co.uk>
2025-11-08 15:34:15 +0000

    refactor: Update logger initialization to use environment-based configuration and streamline error handling

--- pkg/commonlibrary/logger/logger.go
+++ pkg/commonlibrary/logger/logger.go
@@ -3,6 +3,7 @@ package logger
 import (
 	"fmt"
 	"os"
+	"strings"
 
 	"go.uber.org/zap"
 
@@ -10,24 +11,26 @@ import (
 )
 
 func New(cfg *config.Config) *zap.Logger {
-	var logger *zap.Logger
+	var (
+		logger *zap.Logger
+		err    error
+	)
 
-	var err error
+	switch strings.ToLower(cfg.Env) {
+	case "local", "development", "dev", "test":
+		devConfig := zap.NewDevelopmentConfig()
+		devConfig.OutputPaths = []string{"stdout"}
+		devConfig.ErrorOutputPaths = []string{"stdout"}
 
-	if cfg.Env == "prod" {
+		logger, err = devConfig.Build()
+	default:
 		logger, err = zap.NewProduction()
-	} else {
-		logger, err = zap.NewDevelopment()
 	}
 
 	if err != nil {
 		fmt.Printf("Failed to create logger: %v\n", err)
 		os.Exit(1)
 	}
-
-	defer func() {
-		_ = logger.Sync()
-	}() // flushes any buffered log entries
 
 	return logger
 }
