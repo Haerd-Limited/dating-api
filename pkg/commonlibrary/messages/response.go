0be413f3fb0c23fbde68d1b9f6aa6a12d25bf0b9
lionel.wilson <lwilson@cynergybank.co.uk>
2025-11-28 11:08:56 +0000

    fix: Update blocked tokens and refine social media restriction message for improved clarity and consistency

--- pkg/commonlibrary/messages/response.go
+++ pkg/commonlibrary/messages/response.go
@@ -24,6 +24,6 @@ const (
 	RequestCancelledMsg           = "Request cancelled"
 	AllFieldsRequiredMsg          = "All fields are required"
 	InvalidRequestBodyMsg         = "Failed to validate and decode request body. Please double check your request body"
-	SocialsNotAllowedMsg          = "To keep Haerd personality-first, socials aren’t allowed in your display name, job title, work place or university. Please remove @handles, slashes, links, or “dot com” text."
+	SocialsNotAllowedMsg          = "To keep Haerd personality-first, socials aren’t allowed in your display name, job title, workplace or university. Please remove @handles, slashes, links, “dot com” text or any requests to follow or message you."
 	VoiceWorthHearingSearchingMsg = "Still looking for the best people for you"
 )
